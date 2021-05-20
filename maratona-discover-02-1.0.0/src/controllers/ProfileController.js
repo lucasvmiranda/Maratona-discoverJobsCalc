54996a81b9397e7d9de5d00c7d61535fb3116189
Lucas <lucasdvd2@gmail.com>
2021-05-20 19:19:13 -0300

    aula4

--- maratona-discover-02-1.0.0/src/controllers/ProfileController.js
+++ maratona-discover-02-1.0.0/src/controllers/ProfileController.js
@@ -1,11 +1,11 @@
 const Profile = require('../model/Profile')
 
 module.exports = {
-    index(req, res) {
-        return res.render("profile", { profile: Profile.get()})
+   async index(req, res) {
+        return res.render("profile", { profile: await Profile.get()})
     },
 
-    update(req, res) {
+    async update(req, res) {
         // req.body para pegar os dados
         const data = req.body
         // definir quantas semanas tem num ano: 52
@@ -22,8 +22,10 @@ module.exports = {
         // quantas horas por semana estou trabalhando
         const valueHour = data["monthly-budget"] / monthlyTotalHours
 
-        Profile.update({
-            ...Profile.get(),
+        const profile = await Profile.get()
+        
+        await Profile.update({
+            ... profile,
             ...req.body,
             "value-hour": valueHour
         }) 
